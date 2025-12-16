efab19557764d534a3eec1b98cf7cff36dc95f12
Jenny-flex <jennyooi@flexsoftware.com.my>
2025-12-16 10:46:00 +0800

    slotusage model

---
+++ RackingSystem/Models/Slot/SlotUsageDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RackingSystem.Models.Slot
+{
+    public class SlotUsageDTO
+    {
+        public string title { get; set; } = String.Empty;
+        public bool available { get; set; } = false;
+        public int slotQty { get; set; } = 0;
+        public double percentage { get; set; } = 0.00;
+         
+    }
+}
