8699ef17e447f1f9ade6c3cde2f566334a783e7f
Jenny-flex <jennyooi@flexsoftware.com.my>
2026-06-09 13:59:33 +0800

    update for font style and css file

---
+++ RackingSystem/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./**/*.{razor,cshtml,html}",
+        "./Pages/**/*.{razor,cshtml}",
+        "./Components/**/*.{razor}",
+        "./wwwroot/**/*.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
